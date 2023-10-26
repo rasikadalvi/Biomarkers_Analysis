63e24c1427ef10f229a4d65cf37f9a8384f217e7
rasikadalvi <145513403+rasikadalvi@users.noreply.github.com>
2023-10-26 13:43:32 +0000

    Created R file for the project

---
+++ s2511090.r
@@ -0,0 +1,9 @@
+## Introduction to Probability & Statistics
+## Project 
+## Student number- s2511090
+
+## Hypothesis testing question - 
+## Regression Modelling - 
+
+# Install and load necessary libraries if not already installed
+
